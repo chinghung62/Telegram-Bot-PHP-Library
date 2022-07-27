114ea625d84d6136c05a1d0663a9fbc19cc12dfa
Ching Hung Tan <103587066+chinghung2000@users.noreply.github.com>
2022-07-28 01:23:21 +0800

    Add files via upload

--- Code Examples/HelloBot.php
+++ Code Examples/HelloBot.php
@@ -1,23 +1,23 @@
 <?php
 
-// include Telegram Bot library
+// Include Telegram Bot library
 include("telegram-bot-1.5.0.php");
 
-$bot_token = "1103799654:AAGk_HkEI8Dz9QiBAtK3PQxQmWTJ7nt9Lts";
+$bot_token = "<token>";     // replace with your bot's API token
 $bot_username = "HelloBot";
 
-// create Bot object
+// Create Bot object
 $bot = new Bot($bot_token, $bot_username);
 
-// read raw data from the request body
+// Read raw data from the request body
 $php_input = file_get_contents("php://input");
 
-// load update from data
+// Load update from data
 $update = $bot->load_update($php_input);
 
-// bot logic: to respond 'Hi!' when user sends 'hello' to the bot
+// Bot logic: to respond 'Hi!' when user sends 'Hello' to the bot
 if ($update->message) {
-    if ($update->message->text == "hello") {
+    if ($update->message->text == "Hello") {
         $bot->send_message($update->message->chat->id, "Hi!");
     }
 }
